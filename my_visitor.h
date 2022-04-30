59962893cdc701e0e67053912fa20be4ad20d972
DH-Cang <cdh573885@outlook.com>
2022-04-30 05:21:12 +0000

    update vistor and AST

--- my_visitor.h
+++ my_visitor.h
@@ -5,4 +5,8 @@ using namespace antlrcpp;
 
 class my_visitor : public CParserBaseVisitor {
 public:
+    std::vector<std::unique_ptr<FuncDef>> m_func_roots;
+    std::vector<std::unique_ptr<BaseNode>> m_global_vars;
+
+    const BaseNode *tmp_ptr;
 };
