8e3a0087aecde7bdfb74811fda0714dac29a9cca
Locietta <locietta@qq.com>
2022-05-02 17:13:00 +0800

    remove comment in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,3 @@
-/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
- * Use of this file is governed by the BSD 3-clause license that
- * can be found in the LICENSE.txt file in the project root.
- */
-
-//
-//  main.cpp
-//  antlr4-cpp-demo
-//
-//  Created by Mike Lischke on 13.03.16.
-//
 #include <iostream>
 #include <memory>
 
