19e5b0099e28760e6b12983f741c25b4199508e9
Locietta <locietta@qq.com>
2022-05-04 20:33:40 +0800

    add simple interface for IRGen

--- IR/IRGenerator.h
+++ IR/IRGenerator.h
@@ -4,11 +4,17 @@
 
 #include "AST.h"
 
+namespace fs = std::filesystem;
+
 class IRGenerator {
+
 public:
     IRGenerator() = delete;
     IRGenerator(std::vector<std::shared_ptr<Expr>> const &trees);
     IRGenerator(std::vector<std::shared_ptr<Expr>> &&trees);
+
+    void printIR(fs::path const &asm_path) const;
+    void codegen();
 
 private:
     std::vector<std::shared_ptr<Expr>> m_trees;
