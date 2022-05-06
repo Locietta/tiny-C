27e0472228927fcb70c4d3955381aaebcd20e6b0
Locietta <locietta@qq.com>
2022-05-06 23:01:40 +0800

    add `SymbolTable` interface

--- IR/IRGenerator.h
+++ IR/IRGenerator.h
@@ -4,8 +4,22 @@
 
 namespace fs = std::filesystem;
 
-class IRGenerator {
+class SymbolTable {
+public:
+    void push();
+    void pop();
 
+    [[nodiscard]] bool contains(llvm::StringRef var_name) const;
+
+    llvm::Value *&operator[](llvm::StringRef var_name);
+    llvm::Value *const &operator[](llvm::StringRef var_name) const;
+
+private:
+    llvm::StringMap<llvm::Value *> globals;                      // GlobalVariable *
+    llvm::SmallVector<llvm::StringMap<llvm::Value *>, 8> locals; // llvm::AllocaInst *
+};
+
+class IRGenerator {
 public:
     IRGenerator() = delete;
     IRGenerator(std::vector<std::shared_ptr<Expr>> const &trees);
@@ -14,19 +28,14 @@ public:
     void codegen();
 
 private:
-    llvm::Value *visitAST(const Expr &expr);
+    llvm::Value *visitASTNode(const Expr &expr);
     std::vector<std::shared_ptr<Expr>> const &m_trees; // reference to ASTBuilder
 
     std::unique_ptr<llvm::LLVMContext> m_context;
     std::unique_ptr<llvm::Module> m_module;
     std::unique_ptr<llvm::IRBuilder<>> m_builder;
 
-    llvm::StringMap<llvm::AllocaInst *> m_symbolTable;
-    std::map<std::string, enum DataTypes> m_varTypeTable;
-
-    bool m_is_global = true;
+    SymbolTable m_symbolTable;
 
     llvm::Type *getLLVMType(enum DataTypes);
-    llvm::AllocaInst *CreateEntryBlockAlloca(llvm::Function *TheFunction, llvm::StringRef VarName,
-                                             llvm::Type *type);
 };
