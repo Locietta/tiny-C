f3d39076b1ae50a7e29bdba2cd02931f87efb3bd
Locietta <locietta@qq.com>
2022-06-17 13:35:19 +0800

    linker: simplify linker script

--- linker.sh
+++ linker.sh
@@ -4,6 +4,6 @@ shift
 
 ld.lld -pie --eh-frame-hdr -m elf_x86_64 -dynamic-linker /lib64/ld-linux-x86-64.so.2\
  -o $first \
-/usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/../../../../lib64/Scrt1.o /usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/../../../../lib64/crti.o /usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/crtbeginS.o -L/usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0 -L/usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/../../../../lib64 -L/lib/../lib64 -L/usr/lib/../lib64 -L/usr/bin/../lib -L/lib -L/usr/lib \
+/usr/lib64/Scrt1.o /usr/lib64/crti.o /usr/lib64/gcc/x86_64-pc-linux-gnu/12.1.0/crtbeginS.o -L/usr/lib64/gcc/x86_64-pc-linux-gnu/12.1.0 -L/usr/lib64 -L/lib64 -L/usr/lib -L/lib \
  $* $my_stblib_path \
- -lgcc --as-needed -lgcc_s --no-as-needed -lc -lgcc --as-needed -lgcc_s --no-as-needed /usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/crtendS.o /usr/sbin/../lib64/gcc/x86_64-pc-linux-gnu/12.1.0/../../../../lib64/crtn.o
+ -lgcc --as-needed -lgcc_s --no-as-needed -lc -lgcc --as-needed -lgcc_s --no-as-needed /usr/lib64/gcc/x86_64-pc-linux-gnu/12.1.0/crtendS.o /usr/lib64/crtn.o
